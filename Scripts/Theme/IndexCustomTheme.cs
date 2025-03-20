962e63ddf73f89dc941ccdc4d92b87246ec16d03
Zayne <142189531+zaynethedev@users.noreply.github.com>
2025-03-19 18:30:06 -0700

    platforms

---
+++ Scripts/Theme/IndexCustomTheme.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+[AddComponentMenu("Index Customization/Index Custom Theme")]
+public class IndexCustomTheme : MonoBehaviour
+{
+    public string ThemeName = "Theme Name";
+    public string ThemeDescription = "Theme Description";
+    public string ThemeAuthor = "Theme Author";
+}
