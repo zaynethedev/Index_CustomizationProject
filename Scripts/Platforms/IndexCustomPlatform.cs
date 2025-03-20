e5ba3d57f5f50a60b087cc273d553d5afd18a055
Zayne <142189531+zaynethedev@users.noreply.github.com>
2025-03-19 18:32:08 -0700

    Delete Scripts directory

--- Scripts/Platforms/IndexCustomPlatform.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-[AddComponentMenu("Index Customization/Index Custom Platform")]
-public class IndexCustomPlatform : MonoBehaviour
-{
-    public string PlatformName = "Platform Name";
-    public string PlatformDescription = "Platform Description";
-    public string PlatformAuthor = "Platform Author";
-}
